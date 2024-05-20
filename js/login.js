00e6bb640b73d4ce1d8f107599ad83ef10ccabc6
IGNACIOb21 <166768975+IGNACIOb21@users.noreply.github.com>
2024-05-20 16:28:18 -0400

    avances actividad 2.1

---
+++ js/login.js
@@ -0,0 +1,18 @@
+
+$(document).ready(function() {
+    $("h1").click(function() {
+        window.location.href = "http://127.0.0.1:5500/index.html";
+    });
+});
+
+
+function loguear(){
+    let user = document.getElementById("usuario").value;
+    let pass = document.getElementById("clave").value;
+
+    if (user == "juen" && pass == "1234"){
+        window.location = "bienvebida.html";
+    } else {
+        alert("El nombre o la contraseña esta incorrecta");
+    } 
+}
