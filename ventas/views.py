93bf57aabc04c055204998e1c29e5bc3532f9084
IGNACIOb21 <166768975+IGNACIOb21@users.noreply.github.com>
2024-06-28 20:17:27 -0400

    nuevo camvip

--- ventas/views.py
+++ ventas/views.py
@@ -3,19 +3,100 @@ from django.shortcuts import render
 def bienvenida(request):
     context = {}
     return render(request, 'inicio/bienvenida.html', context)
-
 def index(request):
     context = {}
     return render(request, 'inicio/index.html', context)
-
 def ofertas(request):
     context = {}
     return render(request, 'inicio/ofertas.html', context)
-
 def registrarse(request):
     context = {}
     return render(request, 'inicio/registrarse.html', context)
-
 def prueba(request):
     context = {}
     return render(request, 'inicio/prueba.html', context)
+
+
+def ventana_gato(request):
+    context = {}
+    return render(request, 'ventanas/ventana_gato.html', context)
+def ventana_recien_llegados(request):
+    context = {}
+    return render(request, 'ventanas/ventana_recien_llegados.html', context)
+def ventana_recien_llegados(request):
+    context = {}
+    return render(request, 'ventanas/ventana_recien_llegados.html', context)
+
+
+
+
+
+def ventaGato1(request):
+    context = {}
+    return render(request, 'gatos/ventaGato1.html', context)
+def ventaGato2(request):
+    context = {}
+    return render(request, 'gatos/ventaGato2.html', context)
+def ventaGato3(request):
+    context = {}
+    return render(request, 'gatos/ventaGato3.html', context)
+def ventaGato4(request):
+    context = {}
+    return render(request, 'gatos/ventaGato4.html', context)
+def ventaGato5(request):
+    context = {}
+    return render(request, 'gatos/ventaGato5.html', context)
+def ventaGato6(request):
+    context = {}
+    return render(request, 'gatos/ventaGato6.html', context)
+def ventaGato7(request):
+    context = {}
+    return render(request, 'gatos/ventaGato7.html', context)
+def ventaGato8(request):
+    context = {}
+    return render(request, 'gatos/ventaGato8.html', context)
+def ventaGato9(request):
+    context = {}
+    return render(request, 'gatos/ventaGato9.html', context)
+def ventaGato10(request):
+    context = {}
+    return render(request, 'gatos/ventaGato10.html', context)
+def ventaGato11(request):
+    context = {}
+    return render(request, 'gatos/ventaGato11.html', context)
+def ventaGato12(request):
+    context = {}
+    return render(request, 'gatos/ventaGato12.html', context)
+def ventaGato13(request):
+    context = {}
+    return render(request, 'gatos/ventaGato13.html', context)
+def ventaGato14(request):
+    context = {}
+    return render(request, 'gatos/ventaGato14.html', context)
+def ventaGato15(request):
+    context = {}
+    return render(request, 'gatos/ventaGato15.html', context)
+def ventaGato16(request):
+    context = {}
+    return render(request, 'gatos/ventaGato16.html', context)
+def ventaGato17(request):
+    context = {}
+    return render(request, 'gatos/ventaGato17.html', context)
+def ventaGato18(request):
+    context = {}
+    return render(request, 'gatos/ventaGato18.html', context)
+def ventaGato19(request):
+    context = {}
+    return render(request, 'gatos/ventaGato19.html', context)
+def ventaGato20(request):
+    context = {}
+    return render(request, 'gatos/ventaGato20.html', context)
+def ventaGato21(request):
+    context = {}
+    return render(request, 'gatos/ventaGato21.html', context)
+def ventaGato22(request):
+    context = {}
+    return render(request, 'gatos/ventaGato22.html', context)
+def ventaGato23(request):
+    context = {}
+    return render(request, 'gatos/ventaGato23.html', context)
